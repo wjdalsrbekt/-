eec3e8665e04895e4a8f0731e21f2afbfe6b8537
정민규 <wjdalsrbekt@gmail.com>
2021-04-26 23:57:06 +0900

    백준 10171 고양이:브론즈5

---
+++ JAVA/백준_10171_고양이.java
@@ -0,0 +1,9 @@
+
+public class 백준_10171_고양이 {
+	public static void main(String[] args) {
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+}
